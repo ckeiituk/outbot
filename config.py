7d3f938e82ff8f8ccd0a9d4342a15f209f4bb514
meur <ckeiituk@gmail.com>
2025-10-04 00:51:30 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,15 @@
+# Настройки подключения к базе данных
+DATABASE_CONFIG = {
+    'user': 'REDACTED_DB_USER',
+    'password': 'REDACTED_DB_PASSWORD',
+    'database': 'REDACTED_DB_NAME',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+bot_token2 = 'REDACTED_DISCORD_TOKEN'
+bot_token = 'REDACTED_ALT_TOKEN'
+faceit_api_key = 'REDACTED_FACEIT_API'
+
+GUILD_ID = 233981443766878208
+CHANNEL_ID = 1199361664361963540
